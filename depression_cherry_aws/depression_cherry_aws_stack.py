1272771158c59cfa6753d6b23c0ace7f7d4bd525
James McNeill <245873540+JamesMcNeill2@users.noreply.github.com>
2026-07-17 21:28:22 +0100

    Added Log Group

--- depression_cherry_aws/depression_cherry_aws_stack.py
+++ depression_cherry_aws/depression_cherry_aws_stack.py
@@ -1,6 +1,7 @@
 from aws_cdk import (
     Stack,
     Duration,
+    RemovalPolicy,
     aws_lambda as _lambda,
     aws_logs as logs
 )
@@ -11,6 +12,12 @@ class DepressionCherryAwsStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, env_name: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
+        log_group = logs.LogGroup(
+            self, "NasaLogGroup",
+            retention=logs.RetentionDays.ONE_WEEK,
+            removal_policy=RemovalPolicy.DESTROY,
+        )
+
         _lambda.Function(
             self, "Nasa",
             function_name=f"Nasa-{env_name}",
@@ -18,5 +25,5 @@ class DepressionCherryAwsStack(Stack):
             handler="nasa.lambda_handler",
             code=_lambda.Code.from_asset("lambda"),
             timeout=Duration.seconds(120),           # 2 min
-            log_retention=logs.RetentionDays.ONE_WEEK
+            log_group=log_group
             )
