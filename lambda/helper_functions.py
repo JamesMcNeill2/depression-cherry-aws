fd7ec832d1d5ae6af20674e6a9c85be0103cb085
James McNeill <mcneillj26@gmail.com>
2026-07-17 22:47:07 +0100

    Copied depression-cherry code across

---
+++ lambda/helper_functions.py
@@ -0,0 +1,103 @@
+import os
+import time
+import smtplib
+import logging
+from datetime import datetime
+from email.message import EmailMessage
+import requests
+
+def configure_logging():
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s [%(levelname)s] %(message)s",
+        handlers=[logging.StreamHandler()]
+    )
+
+def get_api_response(url, max_retries=5):
+    logging.info("Getting api key")
+    api_key = os.getenv("API_KEY")
+    if api_key is not None:
+        logging.info("API_KEY has been retrieved")
+        params = {"api_key": api_key}
+    else:
+        logging.error("API_KEY has not been retrieved")
+        raise Exception("API_KEY has not been retrieved")
+
+
+    for attempt in range(max_retries):
+        logging.info(f"Running api request. Attempt: {attempt+1}")
+        response = requests.get(url, params=params)
+        status_code = response.status_code
+        
+        if status_code in (429, 500, 502, 503, 504):
+            retry_after = response.headers.get("Retry-After")
+            wait = int(retry_after) if retry_after else 2 ** attempt
+            logging.info(f"{status_code} error: Retrying in {wait}s. Attempt {attempt+1}/{max_retries}")
+            time.sleep(wait)
+            continue
+        
+        response.raise_for_status()
+        logging.info(f"Status Code: {response.status_code}")
+        return response
+        
+    error_msg = f"Unable to access api after max retries: {max_retries}"
+    logging.error(error_msg)
+    raise Exception(error_msg)
+
+def get_img(data):
+    logging.info("Getting image")
+    url = data["url"]
+    img_response = requests.get(url)
+    img_response.raise_for_status()
+    logging.info("Image received")
+    return img_response.content
+
+def get_env_values():
+    env_values = {
+        "EMAIL_FROM": os.getenv("EMAIL_FROM"),
+        "EMAIL_TO": os.getenv("EMAIL_TO"),
+        "EMAIL_PASSWORD": os.getenv("EMAIL_PASSWORD")
+    }
+    
+    missing = [name for name, value in env_values.items() if not value]
+    if missing:
+        raise ValueError(f"Missing required env values: {', '.join(missing)}")
+
+    logging.info("All email env values retrieved")
+    return env_values["EMAIL_FROM"], env_values["EMAIL_TO"], env_values["EMAIL_PASSWORD"]
+
+def send_email(data, img_bytes):
+    title, date, explanation = data["title"], data["date"], data["explanation"]
+    formatted_date = datetime.strptime(date, "%Y-%m-%d").strftime("%d %B %Y")
+    cid = "nasa_image"
+    
+    email_from, email_to, password = get_env_values()
+    
+    msg = EmailMessage()
+    msg["Subject"] = f"{formatted_date}: {title}"
+    msg["From"] = email_from
+    msg["To"] = email_to
+    # msg.set_content(explanation)
+    # msg.add_attachment(img_bytes, maintype="image", subtype="jpeg", filename="nasa_img.jpg")
+    
+    html_body = f"""
+      <html>
+        <body>
+        <h2>{title}</h2>
+        <img src="cid:{cid}" style="max-width:100%; height:auto;">
+        <h2>Explanation</h2>
+        <p>{explanation}</p>
+      </body>
+    </html>
+    """
+    
+    msg.add_alternative(html_body, subtype="html")
+    
+    html_part = msg.get_payload()[-1]
+    html_part.add_related(img_bytes, maintype="image", subtype="jpeg", cid=f"<{cid}>")
+
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
+        logging.info("Sending email")
+        server.login(email_from, password)
+        server.send_message(msg)
+        logging.info("Email sent")
--- lambda/nasa.py
+++ lambda/nasa.py
@@ -1,6 +1,11 @@
+from helper_functions import configure_logging, get_api_response, get_img, send_email
+
 def lambda_handler(event, context):
 
-    return {
-        "statusCode": 200,
-        "body": "Hello from Lambda"
-    }
+    configure_logging()
+
+    url = "https://api.nasa.gov/planetary/apod"
+    nasa_data = get_api_response(url).json()
+    send_email(nasa_data, get_img(nasa_data))
+
+lambda_handler({}, {})
