b026087242f7da069bbdb8d65ef324bd7d25bf66
James McNeill <mcneillj26@gmail.com>
2026-07-18 15:42:34 +0100

    Added get_params()

--- lambda/nasa.py
+++ lambda/nasa.py
@@ -1,8 +1,16 @@
-from helper_functions import configure_logging, get_api_response, get_img, send_email
+from helper_functions import configure_logging, get_params, get_api_response, get_img, send_email
 
 def lambda_handler(event, context):
 
     configure_logging()
+
+    params = get_params()
+    nasa_api_key = params["nasa-api-key"]
+    gmail_password = params["gmail-password"]
+    email_from = params["email-from"]
+    email_to = params["email-to"]
+
+    print(email_from)
 
     url = "https://api.nasa.gov/planetary/apod"
     nasa_data = get_api_response(url).json()
