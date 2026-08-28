88f886e5133bdb56eae2321caeeb00c6f5e71c26
James McNeill <245873540+JamesMcNeill2@users.noreply.github.com>
2026-08-28 19:28:07 +0100

    Added comments to nasa.py

--- lambda/nasa.py
+++ lambda/nasa.py
@@ -1,9 +1,15 @@
-from helper_functions import configure_logging, get_params, get_api_response, get_img, send_email
+from helper_functions import configure_logging, get_api_response, get_img, send_email
 
 def lambda_handler(event, context):
 
+    # Sets up the logger
     configure_logging()
 
+    # Defines the Nasa API URL
     url = "https://api.nasa.gov/planetary/apod"
+
+    # Fetches today's NASA APOD data
     nasa_data = get_api_response(url).json()
+
+    # Emails the title, photo and description
     send_email(nasa_data, get_img(nasa_data))
