2b2dbd9876b9ef814ccf79eb66e6eebdecf321fb
James McNeill <245873540+JamesMcNeill2@users.noreply.github.com>
2026-09-02 16:33:17 +0100

    Ruff updates to nasa.py

--- lambda/nasa.py
+++ lambda/nasa.py
@@ -1,6 +1,13 @@
-from helper_functions import configure_logging, get_params, get_api_response, get_img_url, get_img, send_email
+from helper_functions import (
+    configure_logging,
+    get_api_response,
+    get_img,
+    get_img_url,
+    get_params,
+    send_email,
+)
 
-# Defines the Nasa API URL  
+# Defines the Nasa API URL
 URL = "https://api.nasa.gov/planetary/apod"
 
 def lambda_handler(event, context):
